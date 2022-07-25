8d516ae155bf2271b43f85d0d111e76ae2b4f916
github username <github email address>
2022-07-25 13:58:42 -0500

    added navbar component with pages

---
+++ src/components/Navbar.js
@@ -0,0 +1,38 @@
+import React from "react"
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  NavLink, 
+} from "react-router-dom";
+import Home from "../components/Home"
+import FirstDoses from "../components/FirstDoses"
+import YourDoses from "../components/YourDoses"
+
+const Navbar = () => {
+  return (
+    <div>
+      <Router>
+          <div className="navbar-container">
+            <h1 className="navbar-logo">First Dose Fulfillment</h1>
+              <div className="nav-links">
+                <NavLink exact to="/" className="main-nav" activeClassName="main-nav-active">Home</NavLink>
+                <NavLink exact to="/FirstDoses" className="main-nav" activeClassName="main-nav-active">FirstDoses</NavLink>
+                <NavLink exact to="/YourDoses" className="main-nav" activeClassName="main-nav-active">YourDoses</NavLink>
+                {/* {props.user && <LogoutButton/>} */}
+              </div>
+          </div>
+
+
+          <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/FirstDoses" element={<FirstDoses/>}/>
+            <Route path="/YourDoses" element={<YourDoses/>}/>
+              
+          </Routes>
+      </Router> 
+    </div>
+  )
+}
+
+export default (Navbar)
