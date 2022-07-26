4825feedc7efb4253b8c3f84addc7a2e33aa1dd1
jtledbet <jon.ledbetter@gmail.com>
2022-07-26 16:02:55 -0400

    fixed about statement, made it pop

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,10 @@
 import React from "react"
 import HomeContainer from "../containers/HomeContainer"
-// import {ThemeProvider, Container, Row, Col} from 'react-bootstrap'
 
 const Home = () => {
   return (
     <div className="home-component">
-      <HomeContainer>
-      </HomeContainer>
+      <HomeContainer/>
     </div>
   )
 }
