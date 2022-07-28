dedee5e90aba4a14aa81e27073558239f0e81cb6
github username <github email address>
2022-07-28 12:54:40 -0500

    added state values for sample view

---
+++ src/containers/SampleView.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const SampleView = (props) => {
+  
+  const {sample_name, image_url, description, sample_size} = props.selectedSample 
+  
+  return(
+    <div>
+      <h1 style={{paddingTop: "20px", color: "whitesmoke", fontFamily: "Cinzel", fontWeight: "bold"}}>{sample_name}</h1>
+      <img src={image_url} alt={sample_name}/>
+      <div className="sample-info-container">
+        <h4 style={{fontFamily: "Cinzel"}}>{description}</h4>
+        <h4 style={{fontFamily: "Cinzel"}}>Sample Size: {sample_size}</h4>
+      </div>
+    </div>
+  )
+}
+
+export default (SampleView)
