38d5d6ef3bffb58cac5914492f72f92c2aeb64db
jtledbet <jon.ledbetter@gmail.com>
2022-08-03 15:06:00 -0400

    further stylization of the Your Doses page

--- src/components/YourDoses.js
+++ src/components/YourDoses.js
@@ -9,66 +9,70 @@ const YourDoses = () => {
 
   return (
     <div className="your-doses-page">
-      <div className="yourdose-explain-container">
-        <div id="yourdose-explain-header">
-          Check
+      <div className="yourdose-contract-frame">
+        <div className="yourdose-explain-container">
+          <div id="yourdose-explain-header">
+            Check
+          </div>
+          <div id="yourdose-explain-body">
+            the status of your contract.  First Dose Fulfillment uses Hellosign to easily send contracts right to your email.  Once you sign, your first dose is on the way.  Keep track with the table below.
+          </div>
         </div>
-        <div id="yourdose-explain-body">
-          the status of your contract.  First Dose Fulfillment uses Hellosign to easily send contracts right to your email.  Once you sign, your first dose is on the way.  Keep track with the table below.
-        </div>
+        <Table striped bordered hover variant="dark" id="contract-table">
+          <thead>
+            <tr>
+              <th>Quantity</th>
+              <th>Sample Name</th>
+              <th>Status</th>
+              <th>Date Ordered</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              yourDoses.map((dose) => (
+                <tr key={dose.id}>
+                  <td>{dose.quantity} {dose.quantity === 1 ? "dose" : "orders"}</td>
+                  <td>{dose.sample_name}</td>
+                  <td>{dose.status}</td>
+                  <td>{dose.status_datetime}</td>
+                </tr>
+              ))
+            }
+          </tbody>
+        </Table>
       </div>
-      <Table striped bordered hover variant="dark" id="contract-table">
-        <thead>
-          <tr>
-            <th>Quantity</th>
-            <th>Sample Name</th>
-            <th>Status</th>
-            <th>Date Ordered</th>
-          </tr>
-        </thead>
-        <tbody>
-          {
-            yourDoses.map((dose) => (
-              <tr key={dose.id}>
-                <td>{dose.quantity} {dose.quantity === 1 ? "dose" : "orders"}</td>
-                <td>{dose.sample_name}</td>
-                <td>{dose.status}</td>
-                <td>{dose.status_datetime}</td>
-              </tr>
-            ))
-          }
-        </tbody>
-      </Table>
-      <div className="shipping-explain-container">
-        <div id="shipping-explain-header">
-          Track
+      <div className="yourdose-shipping-frame">
+        <div className="shipping-explain-container">
+          <div id="shipping-explain-header">
+            Track
+          </div>
+          <div id="yourdose-explain-body">
+            your First Dose order.  First Dose uses ShipStation to provide up-to-date information on your current orders. Click on an order below to see its status.
+          </div>
         </div>
-        <div id="yourdose-explain-body">
-          your First Dose order.  First Dose uses ShipStation to provide up-to-date information on your current orders. Click on an order below to see its status.
-        </div>
+        <Table striped bordered hover variant="dark" id="shipping-table">
+          <thead>
+            <tr>
+              <th>Quantity</th>
+              <th>Sample Name</th>
+              <th>Status</th>
+              <th>Date Ordered</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              yourDoses.map((dose) => (
+                <tr key={dose.id}>
+                  <td>{dose.quantity} {dose.quantity === 1 ? "dose" : "orders"}</td>
+                  <td>{dose.sample_name}</td>
+                  <td>{dose.status}</td>
+                  <td>{dose.status_datetime}</td>
+                </tr>
+              ))
+            }
+          </tbody>
+        </Table>
       </div>
-      <Table striped bordered hover variant="dark" id="shipping-table">
-        <thead>
-          <tr>
-            <th>Quantity</th>
-            <th>Sample Name</th>
-            <th>Status</th>
-            <th>Date Ordered</th>
-          </tr>
-        </thead>
-        <tbody>
-          {
-            yourDoses.map((dose) => (
-              <tr key={dose.id}>
-                <td>{dose.quantity} {dose.quantity === 1 ? "dose" : "orders"}</td>
-                <td>{dose.sample_name}</td>
-                <td>{dose.status}</td>
-                <td>{dose.status_datetime}</td>
-              </tr>
-            ))
-          }
-        </tbody>
-      </Table>
       <div className="shipping-info-data">
         I'm however we decide to set up the data for shipping
       </div>
