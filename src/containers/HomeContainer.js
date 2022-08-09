7e3975131edb74f99bf9d690a47f53c9cbddcc22
jtledbet <jon.ledbetter@gmail.com>
2022-08-09 15:23:05 -0400

    merge conflicts resolved

--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -1,9 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from 'react-bootstrap'
 import { NavLink } from "react-router-dom"
-import { NavLink} from "react-router-dom"
-import ContactFooter from './ContactFooter'
-import { NavLink} from "react-router-dom"
 import ContactFooter from './ContactFooter'
 
 const HomeContainer = () => {
